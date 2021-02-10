074572c42cdeb0c11eb19897389b5c1138a67f0b
vagrant <gio_wan@live.ca>
2021-02-10 16:09:03 +0000

    set up server side script to play snake

---
+++ play.js
@@ -0,0 +1,34 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+
+const conn = net.createConnection({ 
+  host: 'localhost',
+  port: 50541
+});
+
+// interpret incoming data as text
+conn.setEncoding('utf8'); 
+
+
+
+conn.on('data', (data) => {
+  console.log('Server says: ', data);
+});
+
+
+console.log('Connecting ...');
+connect();
