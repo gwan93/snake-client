478de85a0992a6dc350ad889d0cc1cd912178a19
vagrant <gio_wan@live.ca>
2021-02-10 16:55:12 +0000

    implemented user functionality to manually shut down client

--- play.js
+++ play.js
@@ -1,3 +1,27 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.stdout.write('\nShutting down client.!\n');
+      process.exit();
+    }
+  };
+  
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+}
+
+
+
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
